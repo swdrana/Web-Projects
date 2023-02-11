c1a994c2dfc34c5e394f5b8fe5f4ea37c35dfbae
swdrana <tctr2s@gmail.com>
2023-02-11 15:49:19 +0600

    router dom installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Nav from "./components/Nav";
 function App() {
   return (
     <div className="App">
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a>
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2 bg-base-100">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Nav/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
