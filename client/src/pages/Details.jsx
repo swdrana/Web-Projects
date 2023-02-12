e2b7b444fd76321965fb099c53d724520e692c70
swdrana <tctr2s@gmail.com>
2023-02-12 22:15:51 +0600

    logo added in Details page

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import logo from "../img/logo.png";
 const Details = () => {
-    const {id} = useParams();
-    return (
-        <div>
-            {id}
-        </div>
-    );
+  const { id } = useParams();
+  return (
+    <div className=" container flex items-center content-center flex-col mx-auto">
+        <img className="md:w-1/2 lg:w-1/3" src={logo} alt="" />
+        
+    </div>
+  );
 };
 
 export default Details;
