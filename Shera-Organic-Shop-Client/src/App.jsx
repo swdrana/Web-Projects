c7e925deb53cab73d7e1c16ac489e0a53a9c5b0c
swdrana <tctr2s@gmail.com>
2023-08-25 18:02:00 +0600

    basic web template

--- Shera-Organic-Shop-Client/src/App.jsx
+++ Shera-Organic-Shop-Client/src/App.jsx
@@ -1,11 +1,53 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Shera Organic Shop
-    </h1>
-    </>
-  )
+    <div className=" text-center">
+      <h1 className="  text-5xl my-5 text-green-700 ">
+        Welcome to Shera Organic Shop.
+      </h1>
+      <div className="my-6">
+        <p>
+          বর্তমানে আমাদের website এর কাজ চলছে, খুব তাড়াতাড়ি এখানে update পাবেন। 
+        </p>
+        <p>
+          এখন আমাদের ফেসবুক পেজ থেকে অর্ডার করতে পারেন, আগামী <b>শুক্রবারের</b> মধ্যে আমাদের এই website এর নতুন আপডেট আসবে ইনশাল্লাহ। 
+        </p>
+      </div>
+      <div>
+        <h3 className=" text-2xl underline text-teal-900">Update History</h3>
+        <p>
+          1st Create: 21 Aug 2023 (Lunch Website & give Massege about our
+          Website.)
+        </p>
+        <p>
+          25 Aug 2023: Update Website Security. Now, this website is secure so,
+          you can use happily.
+        </p>
+      </div>
+      <table className=" mx-auto border my-6 text-green-700">
+        <tr className=" border">
+          <th>CEO:</th>
+          <td>Md. Mehedi Hasan</td>
+        </tr>
+        <tr className="border">
+          <th>Phone/ WhatsApp:</th>
+          <td>+8801793143054</td>
+        </tr>
+        <tr className=" border">
+          <th>Facebook:</th>
+          <td>
+            <a href="https://www.facebook.com/sheraorganicshop">
+              https://www.facebook.com/sheraorganicshop
+            </a>
+          </td>
+        </tr>
+      </table>
+      <hr />
+      <div className="text-sm">
+        <h5>Developer: Rana</h5>
+        <p>Whatsapp: 01772651051 Last Update: 25 Aug 20123.</p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
