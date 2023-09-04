608000ade17582304530bcad111b2a344f2575df
swdrana <tctr2s@gmail.com>
2023-09-04 22:43:45 +0600

    single category item design done

--- Shera-Organic-Shop-Client/src/pages/Home/OurTopCategory/TopCategoryCart/TopCategoryCart.jsx
+++ Shera-Organic-Shop-Client/src/pages/Home/OurTopCategory/TopCategoryCart/TopCategoryCart.jsx
@@ -7,17 +7,62 @@ const Img = styled.img`
   padding: 8px;
   border-radius: 50%;
 `;
+const Div = styled.div`
+  position: relative;
+  &:after {
+    content: "";
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    left: 6px;
+    bottom: 9px;
+    background-color: ${(props) => (props.dotColor ? props.dotColor : "red")};
+    border-radius: 50%;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    right: 5px;
+    top: 12px;
+    background-color: ${(props) => (props.dotColor ? props.dotColor : "red")};
+    border-radius: 50%;
+  }
+`;
+const ItemNo = styled.p`
+  /* color:${(props) => (props.dotColor ? props.dotColor : "white")}; */
+  &:before {
+    content: "";
+    display: inline-block;
+    width: 8px;
+    height: 8px;
+    margin-right: 7px;
+    background-color: ${(props) => (props.dotColor ? props.dotColor : "red")};
+    border-radius: 50%;
+    transform: translateY(-1px);
+  }
+`;
 const TopCategoryCart = () => {
+  const dotColor = "#6eb356";
+  const catIcon =
+    "https://grostore.themetags.com/public/uploads/media/WQJjaqXZXNnUHaubvRhHh5c3m4knHkCSwHWCIPWd.png";
+  const catName = "Cat 21";
+  const catItemNo = 1;
   return (
     <div>
-      <div className=" w-44 h-52 border rounded-lg flex items-center justify-center flex-col">
-        <Img
-          src="https://grostore.themetags.com/public/uploads/media/WQJjaqXZXNnUHaubvRhHh5c3m4knHkCSwHWCIPWd.png"
-          alt=""
-          className=" image-full"
-        ></Img>
-        <h2 className=" text-md font-bold mt-2 mb-1">Cat 1</h2>
-        <p>3 Items</p>
+      <div
+        className=" w-44 h-52 border rounded-lg flex items-center justify-center flex-col
+       
+      "
+      >
+        <Div dotColor={dotColor}>
+          <Img src={catIcon} alt={catIcon} className=" image-full"></Img>
+        </Div>
+        <h2 className=" text-md font-bold mt-2 mb-1">{catName}</h2>
+        <ItemNo dotColor={dotColor}>{`${catItemNo} ${
+          catItemNo == 1 ? "Item" : "Items"
+        }`}</ItemNo>
       </div>
     </div>
   );
