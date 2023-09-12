56df6ac3b3a7dbeaf0fca051e2d951979dcf6cce
swdrana <tctr2s@gmail.com>
2023-09-12 19:14:26 +0600

    TB Prodect card ready

--- Shera-Organic-Shop-Client/src/pages/Home/TrendingProducts/TBProductCard.jsx
+++ Shera-Organic-Shop-Client/src/pages/Home/TrendingProducts/TBProductCard.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 const OfferBatch = styled.span`
- &before{
+  &before {
     content: "";
     /* position: absolute;
     right: -1px;
@@ -11,105 +12,97 @@ const OfferBatch = styled.span`
     background-color: #fff;
     -webkit-clip-path: polygon(100% 0, 0 50%, 100% 100%);
     clip-path: polygon(100% 0, 0 50%, 100% 100%); */
- }
-`
+  }
+`;
 const TBProductCard = () => {
-    
   return (
-    <div>
-      <div className=" w-80 border  border-[#f4f4f4] rounded-md col-xxl-3 col-lg-4 col-md-6 col-sm-10 filter_item 7 13 14 16" >
-      {/* style="position: absolute; left: 336px; top: 497.289px;" */}
-        <div className="vertical-product-card rounded-2 relative ">
-          <OfferBatch className=" bg-[#ff0406] p-1 rounded-tl-xl rounded-br-xl text-white absolute top-1 left-1">
+    <div className=" w-[321px] border border-[#f4f4f4] rounded-md hover:shadow-xl  transition-all duration-1000 hover:border-white">
+      <div className=" group w-80">
+        <div className=" rounded-2 relative">
+          <OfferBatch className="  bg-error p-1 rounded-tl-xl rounded-br-xl text-white absolute top-1 left-1">
             -10% <span className="text-uppercase">Off</span>
           </OfferBatch>
 
-          <div className="thumbnail position-relative text-center p-4">
+          <div className="relative text-center p-4">
             <img
               src="https://grostore.themetags.com/public/uploads/media/QRrqV5dcT0zLMPR7wNbRGGkSk9f16V3amjaKk4R8.png"
               alt="Dalim (Pomegranate)"
               className="img-fluid"
             />
-            <div className="product-btns position-absolute d-flex gap-2 flex-column">
-              <a
+            <div className="absolute top-0 right-0 flex gap-2  flex-col">
+              <button
                 href="javascript:void(0);"
                 className="rounded-btn"
                 onClick="showProductDetailsModal(21)"
               >
-                <i className="fa-regular fa-eye"></i>
-              </a>
+                Eye<i className="fa-regular fa-eye"></i>
+              </button>
             </div>
           </div>
 
-          <div className="card-content">
-            <span
-              className="fs-xxs fw-bold"
-              data-bs-toggle="tooltip"
-              data-bs-placement="top"
-              data-bs-title="Reward Points"
-            >
-              <i className="fas fa-medal"></i> 100
-            </span>
+          <div className="px-8  py-6">
             {/* <!--product category start--> */}
-            <div className="mb-2 tt-category tt-line-clamp tt-clamp-1">
-              <a
-                href=""
-                className="d-inline-block text-muted fs-xxs"
-              >
+            <div className="  text-gray-light text-[12px] mb-2">
+              <a href="" className="d-inline-block text-muted fs-xxs">
                 Breakfast ,
               </a>
-              <a
-                href=""
-                className="d-inline-block text-muted fs-xxs"
-              >
+              <a href="" className="d-inline-block text-muted fs-xxs">
                 Fresh Organic ,
               </a>
-              <a
-                href=""
-                className="d-inline-block text-muted fs-xxs"
-              >
+              <a href="" className="d-inline-block text-muted fs-xxs">
                 Fresh Fruits ,
               </a>
-              <a
-                href=""
-                className="d-inline-block text-muted fs-xxs"
-              >
+              <a href="" className="d-inline-block text-muted fs-xxs">
                 Vegetables
               </a>
             </div>
             {/* <!--product category end--> */}
 
-            <a
-              href=""
-              className="card-title fw-bold mb-2 tt-line-clamp tt-clamp-1"
-            >
+            <Link to="" className=" font-bold mb-2">
               Dalim (Pomegranate)
-            </a>
-
-            <h6 className="price">
-              <span className="fw-bold h4 text-danger">₹1,440.00</span>
-
+            </Link>
+            <div className="flex  items-center flex-nowrap mb-2">
+              <div className="rating rating-xs me-2">
+                <input
+                  type="radio"
+                  name="rating-6"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-6"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-6"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-6"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-6"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+              </div>
+              <span className="flex-shrink-0  text-gray-deep text-[12px]">
+                (5.2k Reviews)
+              </span>
+            </div>
+            <h6 className=" font-bold ">
+              <span className="text-md  text-error">₹1,440.00 </span>
               <small>/kg</small>
             </h6>
           </div>
-          <div className="card-btn bg-white">
-            <form action="" className="direct-add-to-cart-form">
-              <input
-                type="hidden"
-                name="_token"
-                value="t0RFZoT9glgvirp7HEeYA2kEmSXHNHb7I40p0ceo"
-              />
-              <input type="hidden" name="product_variation_id" value="28" />
-              <input type="hidden" value="1" name="quantity" />
-
-              <a
-                href="javascript:void(0);"
-                onClick="directAddToCartFormSubmit(this)"
-                className="btn btn-secondary d-block btn-md rounded-1 w-100 direct-add-to-cart-btn add-to-cart-text"
-              >
-                Add to Cart
-              </a>
-            </form>
+          <div className="px-8 pb-6 rounded-b-md duration-1000 transition-all border-2 border-white bg-white absolute w-full group-hover:shadow-xl -bottom-full  group-hover:bottom-auto">
+            <button className="btn btn-secondary w-full text-white hover:btn-primary hover:text-white">
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
--- Shera-Organic-Shop-Client/tailwind.config.js
+++ Shera-Organic-Shop-Client/tailwind.config.js
@@ -1,14 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-light': "var(--gray-light)",
+        'gray-deep': "var(--gray-dark)",
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
-  }
-}
+    themes: [
+      {
+        light: {
+          "primary": "#6eb356",
+          "secondary": "#ff7c08",
+          "accent": "#077765",
+          "neutral": "#2b3440",
+          "base-100": "#ffffff",
+          "info": "#38bdf8",
+          "success": "#22c55e",
+          "warning": "#ffc107",
+          "error": "#ff0406",
+          '--gray-light': '#9b9bb4',
+          "--gray-deep": "#5d6374",
+        },
+      },
+    ],
+  },
+};
