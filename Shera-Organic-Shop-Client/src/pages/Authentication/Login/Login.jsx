127d5504ecc85831801555282af85c32d693ea8d
swdrana <tctr2s@gmail.com>
2023-10-03 17:01:17 +0600

    Working on Login and Registration Page

--- Shera-Organic-Shop-Client/src/pages/Authentication/Login/Login.jsx
+++ Shera-Organic-Shop-Client/src/pages/Authentication/Login/Login.jsx
@@ -1,34 +1,57 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col md:flex-row">
-    <div className="text-center lg:text-left">
+    <div className="text-center lg:text-left w-full lg:w-1/2">
       <h1 className="text-5xl font-bold">Login now!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </div>
-    </div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="card bg-base-100 w-full lg:w-1/2"
+          >
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  {...register("email")}
+                  type="text"
+                  placeholder="youremail@gmail.com"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  {...register("password")}
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+              <div className="form-control mt-6">
+                <Link to='/signup' className=" text-sm underline hover:no-underline">Don't Have an Account? Please Register.</Link>
+              </div>
+            </div>
+          </form>
   </div>
 </div>
         </div>
--- Shera-Organic-Shop-Client/src/pages/Authentication/signup/Signup.jsx
+++ Shera-Organic-Shop-Client/src/pages/Authentication/signup/Signup.jsx
@@ -1,37 +1,87 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
 const Signup = () => {
-    return (
-        <div><div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col md:flex-row-reverse">
-          <div className="text-center lg:text-left">
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col md:flex-row">
+          <div className="text-center lg:text-left  w-full lg:w-1/2">
             <h1 className="text-5xl font-bold">Register now!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
           </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="card bg-base-100 w-full lg:w-1/2"
+          >
             <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Full Name</span>
+                </label>
+                <input
+                  {...register("fullName")}
+                  type="text"
+                  placeholder="your name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Phone</span>
+                </label>
+                <input
+                  {...register("phone")}
+                  type="tel"
+                  placeholder="+8801220000000"
+                  className="input input-bordered"
+                />
+              </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
+                <input
+                  {...register("email")}
+                  type="text"
+                  placeholder="youremail@gmail.com"
+                  className="input input-bordered"
+                />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
+                <input
+                  {...register("password")}
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
                 <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Create Account</button>
+                <button className="btn btn-secondary text-white">Create Account</button>
+              </div>
+              <div className="form-control mt-6">
+                <Link to='/login' className=" text-sm underline hover:no-underline">Have an Account? Please Login.</Link>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Signup;
