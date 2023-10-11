ba50e7f60f583fc01a202fc8db1140ae09076ee9
swdrana <tctr2s@gmail.com>
2023-10-11 19:10:28 +0600

    Product Deltete successfully with photos

--- Shera-Orgamic-Shop-Server/src/routes/productRoutes.ts
+++ Shera-Orgamic-Shop-Server/src/routes/productRoutes.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import multer from 'multer';
 import { Product } from '../models/product';  // Update path accordingly
 
+import fs from 'fs';
+import path from 'path';
+
 const router: Router = Router();
 
 const storage = multer.diskStorage({
@@ -43,31 +46,78 @@ router.post('/', upload.fields([
   }
 });
 
-// router.post('/', upload.fields([
-//   { name: 'productThumbnail', maxCount: 1 },
-//   { name: 'productGallery', maxCount: 10 }
-// ]), async (req, res) => {
-//   try {
-//     if (!req.files) {
-//       return res.status(400).send('No files uploaded.');
-//     }
+// New GET route to fetch all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();  // Fetch all products from the database
+    res.status(200).json(products);  // Send the products as a JSON response
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-//     const productData = {
-//       ...req.body,
-//       productThumbnail: req.files['productThumbnail'] ? req.files['productThumbnail'][0].path : '',
-//       productGallery: req.files['productGallery'] ? req.files['productGallery'].map((file: any) => file.path) : [],
-//       variants: req.body.variants ? JSON.parse(req.body.variants) : [],
-//       isPublished: req.body.isPublished === 'true',
-//     };
+router.put('/:id', async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const updatedProduct = await Product.findByIdAndUpdate(productId, req.body, { new: true });
 
-//     const product = new Product(productData);
+    if (!updatedProduct) {
+      return res.status(404).send('Product not found');
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).send('Product not found');
+    }
+
+    // Function to attempt to delete an image file and log an error if it occurs
+    const deleteImage = (imagePath: string) => {
+
+      // If an image file path exists, try to remove it from the filesystem.
+    if (imagePath) {
+      fs.unlink(imagePath, (err) => {
+        if (err) {
+          console.error("Error deleting the file:", err);
+        }
+      });
+    }
+    };
     
-//     await product.save();
-//     res.status(201).send(product);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
+
+    // Delete Thumbnail
+    if (typeof product.productThumbnail === 'string') {
+      deleteImage(product.productThumbnail);
+    }
+
+    // Delete Gallery Images
+    product.productGallery.forEach(deleteImage);
+
+    // Delete Product from DB
+    await Product.findByIdAndDelete(productId);
+
+    res.status(200).json({ message: 'Product and associated files deleted successfully!' });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
 
 export default router;
