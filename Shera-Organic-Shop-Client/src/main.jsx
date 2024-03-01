245e07f634103b03a8e378c755b4cf55d4b43b58
swdrana <tctr2s@gmail.com>
2024-03-01 12:33:24 +0600

    remove npm uninstall react-messenger-customer-chat

--- Shera-Organic-Shop-Client/src/main.jsx
+++ Shera-Organic-Shop-Client/src/main.jsx
@@ -2,32 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import { router } from "./router/Routes";
-import MessengerCustomerChat from "react-messenger-customer-chat";
-import { QueryClient, QueryClientProvider } from "react-query";
 import AuthProvider from "./provider/AuthProvider";
-import CheckoutProvider from "./provider/CheckoutProvider";
+import {QueryClient, QueryClientProvider} from 'react-query'
 import { ToastProvider } from "./provider/ToastContext";
+import CheckoutProvider from "./provider/CheckoutProvider";
 import AllRoutes from "./router/AllRoutes";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CheckoutProvider>
+      <CheckoutProvider>
         <QueryClientProvider client={queryClient}>
           <AuthProvider>
             <ToastProvider>
               {/* <RouterProvider router={router}/> Object Format */}
-              {/* <AllRoutes /> */}
-      sd
-      <MessengerCustomerChat
-        pageId="105274921383668"
-        appId="1184899329559971"
-      ></MessengerCustomerChat>
-      sd
+              <AllRoutes/>
             </ToastProvider>
           </AuthProvider>
         </QueryClientProvider>
-    </CheckoutProvider>
+      </CheckoutProvider>
   </React.StrictMode>
 );
--- Shera-Organic-Shop-Client/src/pages/Home/Home.jsx
+++ Shera-Organic-Shop-Client/src/pages/Home/Home.jsx
@@ -1,30 +1,28 @@
-import MessengerCustomerChat from "react-messenger-customer-chat";
-import { PageView } from "../../utilities/facebookPixel";
-import BrowseRecentPost from "./BrowseRecentPost/BrowseRecentPost";
-import CustomerReview from "./CustomerReview/CustomerReview";
-import OurFeaturedProducts from "./OurFeaturedProducts/OurFeaturedProducts";
-import OurTopCategory from "./OurTopCategory/OurTopCategory";
-import Slider from "./Slider/Slider";
-import ThreeFeatureImg from "./ThreeFeatureImg/ThreeFeatureImg";
-import TrendingProducts from "./TrendingProducts/TrendingProducts";
-import TwoFeatureImg from "./TwoFeatureImg/TwoFeatureImg";
-import WeeklyBestDeals from "./WeeklyBestDeals/WeeklyBestDeals";
-
+import { PageView } from '../../utilities/facebookPixel';
+import BrowseRecentPost from './BrowseRecentPost/BrowseRecentPost';
+import CustomerReview from './CustomerReview/CustomerReview';
+import OurFeaturedProducts from './OurFeaturedProducts/OurFeaturedProducts';
+import OurTopCategory from './OurTopCategory/OurTopCategory';
+import Slider from './Slider/Slider';
+import ThreeFeatureImg from './ThreeFeatureImg/ThreeFeatureImg';
+import TrendingProducts from './TrendingProducts/TrendingProducts';
+import TwoFeatureImg from './TwoFeatureImg/TwoFeatureImg';
+import WeeklyBestDeals from './WeeklyBestDeals/WeeklyBestDeals';
 const Home = () => {
-  PageView();
-  return (
-    <div>
-      <Slider />
-      <OurTopCategory></OurTopCategory>
-      <OurFeaturedProducts></OurFeaturedProducts>
-      <TrendingProducts></TrendingProducts>
-      <ThreeFeatureImg></ThreeFeatureImg>
-      <WeeklyBestDeals></WeeklyBestDeals>
-      <TwoFeatureImg></TwoFeatureImg>
-      <CustomerReview></CustomerReview>
-      <BrowseRecentPost></BrowseRecentPost>
-    </div>
-  );
+    PageView();
+    return (
+        <div>
+            <Slider/>
+            <OurTopCategory></OurTopCategory>
+            <OurFeaturedProducts></OurFeaturedProducts>
+            <TrendingProducts></TrendingProducts>
+            <ThreeFeatureImg></ThreeFeatureImg>
+            <WeeklyBestDeals></WeeklyBestDeals>
+            <TwoFeatureImg></TwoFeatureImg>
+            <CustomerReview></CustomerReview>
+            <BrowseRecentPost></BrowseRecentPost>
+        </div>
+    );
 };
 
 export default Home;
