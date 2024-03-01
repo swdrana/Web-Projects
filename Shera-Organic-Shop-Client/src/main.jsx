899220bbb60c6773a61a8707ffb85e692984c63c
swdrana <tctr2s@gmail.com>
2024-03-01 12:13:23 +0600

    Messenger Added

--- Shera-Organic-Shop-Client/src/main.jsx
+++ Shera-Organic-Shop-Client/src/main.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import { router } from "./router/Routes";
+import MessengerCustomerChat from "react-messenger-customer-chat";
+import { QueryClient, QueryClientProvider } from "react-query";
 import AuthProvider from "./provider/AuthProvider";
-import {QueryClient, QueryClientProvider} from 'react-query'
-import { ToastProvider } from "./provider/ToastContext";
 import CheckoutProvider from "./provider/CheckoutProvider";
+import { ToastProvider } from "./provider/ToastContext";
 import AllRoutes from "./router/AllRoutes";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <CheckoutProvider>
+    <CheckoutProvider>
         <QueryClientProvider client={queryClient}>
           <AuthProvider>
             <ToastProvider>
               {/* <RouterProvider router={router}/> Object Format */}
-              <AllRoutes/>
+              <AllRoutes />
             </ToastProvider>
           </AuthProvider>
         </QueryClientProvider>
-      </CheckoutProvider>
+    </CheckoutProvider>
   </React.StrictMode>
 );
--- Shera-Organic-Shop-Client/src/pages/Home/Home.jsx
+++ Shera-Organic-Shop-Client/src/pages/Home/Home.jsx
@@ -1,28 +1,34 @@
-import { PageView } from '../../utilities/facebookPixel';
-import BrowseRecentPost from './BrowseRecentPost/BrowseRecentPost';
-import CustomerReview from './CustomerReview/CustomerReview';
-import OurFeaturedProducts from './OurFeaturedProducts/OurFeaturedProducts';
-import OurTopCategory from './OurTopCategory/OurTopCategory';
-import Slider from './Slider/Slider';
-import ThreeFeatureImg from './ThreeFeatureImg/ThreeFeatureImg';
-import TrendingProducts from './TrendingProducts/TrendingProducts';
-import TwoFeatureImg from './TwoFeatureImg/TwoFeatureImg';
-import WeeklyBestDeals from './WeeklyBestDeals/WeeklyBestDeals';
+import MessengerCustomerChat from "react-messenger-customer-chat";
+import { PageView } from "../../utilities/facebookPixel";
+import BrowseRecentPost from "./BrowseRecentPost/BrowseRecentPost";
+import CustomerReview from "./CustomerReview/CustomerReview";
+import OurFeaturedProducts from "./OurFeaturedProducts/OurFeaturedProducts";
+import OurTopCategory from "./OurTopCategory/OurTopCategory";
+import Slider from "./Slider/Slider";
+import ThreeFeatureImg from "./ThreeFeatureImg/ThreeFeatureImg";
+import TrendingProducts from "./TrendingProducts/TrendingProducts";
+import TwoFeatureImg from "./TwoFeatureImg/TwoFeatureImg";
+import WeeklyBestDeals from "./WeeklyBestDeals/WeeklyBestDeals";
+
 const Home = () => {
-    PageView();
-    return (
-        <div>
-            <Slider/>
-            <OurTopCategory></OurTopCategory>
-            <OurFeaturedProducts></OurFeaturedProducts>
-            <TrendingProducts></TrendingProducts>
-            <ThreeFeatureImg></ThreeFeatureImg>
-            <WeeklyBestDeals></WeeklyBestDeals>
-            <TwoFeatureImg></TwoFeatureImg>
-            <CustomerReview></CustomerReview>
-            <BrowseRecentPost></BrowseRecentPost>
-        </div>
-    );
+  PageView();
+  return (
+    <div>
+      <MessengerCustomerChat
+        pageId="105274921383668"
+        appId="1184899329559971"
+      ></MessengerCustomerChat>
+      <Slider />
+      <OurTopCategory></OurTopCategory>
+      <OurFeaturedProducts></OurFeaturedProducts>
+      <TrendingProducts></TrendingProducts>
+      <ThreeFeatureImg></ThreeFeatureImg>
+      <WeeklyBestDeals></WeeklyBestDeals>
+      <TwoFeatureImg></TwoFeatureImg>
+      <CustomerReview></CustomerReview>
+      <BrowseRecentPost></BrowseRecentPost>
+    </div>
+  );
 };
 
 export default Home;
