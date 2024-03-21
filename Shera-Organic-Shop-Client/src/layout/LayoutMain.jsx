b05d5da514839ea70ea9d00a9ff5738bd2535e9b
swdrana <tctr2s@gmail.com>
2024-03-21 22:35:30 +0600

    fb wa btn added

--- Shera-Organic-Shop-Client/src/layout/LayoutMain.jsx
+++ Shera-Organic-Shop-Client/src/layout/LayoutMain.jsx
@@ -1,13 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { FaFacebookMessenger, FaWhatsapp } from "react-icons/fa";
+import { Link, Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
-
 const LayoutMain = () => {
   return (
-    <div>
-      <Header/>
-      <Outlet/>
-      <Footer/>
+    <div className=" relative">
+      <Header />
+      <Outlet />
+      <div className=" fixed bottom-10 right-10 z-50">
+        <div className=" mb-2">
+          <Link target="_blank" to={"https://wa.me/+8801793143054"}>
+            <FaWhatsapp size={30} color="#25D366" />
+          </Link>
+        </div>
+        <div>
+          <Link
+            target="_blank"
+            to={"https://www.facebook.com/messages/t/105274921383668"}
+          >
+            <FaFacebookMessenger size={30} color="#1877F2" />
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
