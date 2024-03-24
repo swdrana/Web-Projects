c323578845525dd003e04e53acfbc3d32f89ffc0
swdrana <tctr2s@gmail.com>
2024-03-24 15:14:36 +0600

    sms icon size increse

--- Shera-Organic-Shop-Client/src/layout/LayoutMain.jsx
+++ Shera-Organic-Shop-Client/src/layout/LayoutMain.jsx
@@ -10,15 +10,17 @@ const LayoutMain = () => {
       <div className=" fixed bottom-12 right-2 md:right-10 z-50">
         <div className=" mb-2">
           <Link target="_blank" to={"https://wa.me/+8801793143054"}>
-            <FaWhatsapp size={30} color="#25D366" />
+            <FaWhatsapp size={40} color="#25D366" />
           </Link>
         </div>
         <div>
           <Link
             target="_blank"
-            to={"https://m.me/sheraorganicshop?text=Hello! I'm coming form your Website."}
+            to={
+              "https://m.me/sheraorganicshop?text=Hello! I'm coming form your Website."
+            }
           >
-            <FaFacebookMessenger size={30} color="#1877F2" />
+            <FaFacebookMessenger size={40} color="#1877F2" />
           </Link>
         </div>
       </div>
